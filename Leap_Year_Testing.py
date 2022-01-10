a556efbdacc17288c0ee2698886837583b49c234
@Gataz <rapenas@gmail.com>
2022-01-10 17:20:39 -0300

    LeapYear Testing

---
+++ Leap_Year_Testing.py
@@ -0,0 +1,40 @@
+﻿# This program will check and test if a year is Leap or not.
+
+# Function setup
+def is_year_leap(year):
+    
+    # Math's condition to tell if a year is a Leap Year or not.
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        return True
+    
+    # If conditions are not met, return False
+    else:
+        return False
+
+# Test Time!
+
+# Given YEARS!
+test_data = [1900, 2000, 2016, 1987]
+
+# Given expected results!
+test_results = [False, True, True, False]
+
+# For each item in test_data
+for i in range(len(test_data)):
+	yr = test_data[i]
+    
+    # Show-me the output this way:...
+    # '1900 -> Failed', or...
+    # ...'1900 -> Ok'
+	print(yr,"->",end="")
+    
+    # Checking if the year is a leap year
+	result = is_year_leap(yr)
+    
+    # Outputs must be 'Failed' for...
+    # ...not corresponding results in test_data[i] and test_results[j]
+    # 1900 is not a Leap year, thus might be False
+	if result == test_results[i]:
+		print("OK")
+	else:
+		print("Failed")
---
+++ README.md
@@ -0,0 +1,17 @@
+Hello There!
+
+This program is intended to check if a year is a leap year or not.
+
+It will Test the coded years also!
+
+In the testing part of the code will return a 'Failed' message if the year do not match with the coded result.
+
+It can be changed as many times you want in the variables 'test_data' (line 17) and 'test_results' (line 20) in a way to have the same number of items (make sure the lenght of both will match) to get the answers you need.
+
+'Ok' stands for leap year and True or NOT leap year and False.
+
+'Failed' stands for leap year and False or NOT leap year and True.
+
+This program is a resulting part of the Cisco Networking Academy pratice exercise.
+
+Have fun!
